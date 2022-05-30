5281bd24eb3aa74fc185ed0ed3d99c594cae9ed4
MENGHAYYU <81160542+Jbot55@users.noreply.github.com>
2022-05-30 20:55:24 +0800

    Update config.js

--- connect/config.js
+++ connect/config.js
@@ -1,7 +1,7 @@
 /**
  - Create By ZackMans Official
- - Contact Me on https://wa.me/+6281385062956
- - Follow iG : @salman_alfarizi_15
+ - Contact Me on https://wa.me/+6288
+ - Follow iG : @junaediiindraa
 */
 
 const fs = require("fs")
@@ -11,17 +11,17 @@ const chalk = require("chalk")
 global.thum = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.log0 = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.err4r = fs.readFileSync("./worker/media/image/thumb.jpg")
-global.owner = ['6289501060783','6289501060783','6289501060783']
+global.owner = ['62895605030379','62895605030379','62895605030379']
 global.rkyt = []
 global.banUser = ['6281366679021@s.whatsapp.net']
 global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
 global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
 global.gcrevoke = ['19054091487-1424650263@g.us']
-global.packname = "Buatan Siapa? Buatan Daffa\nMau nomor nya? 089501060783\nOrangnya ganteng dan baik\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-global.packname2 = "BY Daffa ~ Yudhistira"
-global.author = "WM DAFFA"
+global.packname = "Buatan Siapa? Buatan Daffa\nMau nomor nya? 0895605030379\nOrangnya ganteng dan baik\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.packname2 = "JUNAEDI INDRA"
+global.author = "JUNAEDI INDRA"
 global.sessionName = "session"
-global.linkgrupss = "https://chat.whatsapp.com/KHyGMBYD9OG1lvgGHFYzKy"
+global.linkgrupss = "https://chat.whatsapp.com/KHyGMBYD9O1lvgGHFYzKy"
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.mess = {
